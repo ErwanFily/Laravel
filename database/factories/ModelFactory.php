072bf1f9a78f45176576123f2987fd29fbfb9ec8
ErwanFily <erwanfily@hotmail.fr>
2017-02-19 12:58:46 +0100

    Seeder comment

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -33,3 +33,11 @@ $factory->define(App\Article::class, function (Faker\Generator $faker) {
     ];
 });
 
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'article_id' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
